27fe2076465e747b8df777ba6476502d0d51a028
htomaka <h.tomaka@treeptik.fr>
2014-10-23 11:47:30 +0200

    updated news and home slider

--- js/nav-resp.js
+++ js/nav-resp.js
@@ -1,13 +1,13 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
-	/* prepend menu icon */
-	$('#nav-wrap').prepend('<div id="menu-icon">Menu - <span class="mn-clk">Navigation</span></div>');
-	
-	/* toggle nav */
-	$("#menu-icon").on("click", function(){
-		$("#nav").slideToggle();
-		$(this).toggleClass("active");
-	});
+    /* prepend menu icon */
+    $('#nav-wrap').prepend('<div id="menu-icon">Menu - <span class="mn-clk">Navigation</span></div>');
+
+    /* toggle nav */
+    $("#menu-icon").on("click", function () {
+        $("#nav").slideToggle();
+        $(this).toggleClass("active");
+    });
 
 });
 
